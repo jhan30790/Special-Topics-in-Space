381799c67392cc6a3171dfef2778626a8c302a88
王瑀釩 <jhan30790@gmail.com>
2026-05-11 01:28:20 +0800

    Create to know the form of ran_images_175000.hdf5.py

---
+++ to know the form of ran_images_175000.hdf5.py
@@ -0,0 +1,71 @@
+=== Images ===
+cll_xy/
+  img_175001: shape=(2,), dtype=float64
+  img_175002: shape=(2,), dtype=float64
+  img_175004: shape=(2,), dtype=float64
+  img_175005: shape=(2,), dtype=float64
+  img_175007: shape=(2,), dtype=float64
+  img_175008: shape=(2,), dtype=float64
+  img_175009: shape=(2,), dtype=float64
+input_images: shape=(10, 256, 256), dtype=uint8
+longlat_bounds/
+  img_175001: shape=(4,), dtype=float64
+  img_175002: shape=(4,), dtype=float64
+  img_175004: shape=(4,), dtype=float64
+  img_175005: shape=(4,), dtype=float64
+  img_175007: shape=(4,), dtype=float64
+  img_175008: shape=(4,), dtype=float64
+  img_175009: shape=(4,), dtype=float64
+pix_bounds/
+  img_175001: shape=(4,), dtype=int32
+  img_175002: shape=(4,), dtype=int32
+  img_175004: shape=(4,), dtype=int32
+  img_175005: shape=(4,), dtype=int32
+  img_175007: shape=(4,), dtype=int32
+  img_175008: shape=(4,), dtype=int32
+  img_175009: shape=(4,), dtype=int32
+pix_distortion_coefficient/
+  img_175001: shape=(1,), dtype=float64
+  img_175002: shape=(1,), dtype=float64
+  img_175004: shape=(1,), dtype=float64
+  img_175005: shape=(1,), dtype=float64
+  img_175007: shape=(1,), dtype=float64
+  img_175008: shape=(1,), dtype=float64
+  img_175009: shape=(1,), dtype=float64
+target_masks: shape=(10, 256, 256), dtype=float32
+=== Craters ===
+img_175001/
+  axis0: shape=(6,), dtype=|S14
+  axis1: shape=(9,), dtype=int64
+  block0_items: shape=(6,), dtype=|S14
+  block0_values: shape=(9, 6), dtype=float64
+img_175002/
+  axis0: shape=(6,), dtype=|S14
+  axis1: shape=(28,), dtype=int64
+  block0_items: shape=(6,), dtype=|S14
+  block0_values: shape=(28, 6), dtype=float64
+img_175004/
+  axis0: shape=(6,), dtype=|S14
+  axis1: shape=(71,), dtype=int64
+  block0_items: shape=(6,), dtype=|S14
+  block0_values: shape=(71, 6), dtype=float64
+img_175005/
+  axis0: shape=(6,), dtype=|S14
+  axis1: shape=(3,), dtype=int64
+  block0_items: shape=(6,), dtype=|S14
+  block0_values: shape=(3, 6), dtype=float64
+img_175007/
+  axis0: shape=(6,), dtype=|S14
+  axis1: shape=(111,), dtype=int64
+  block0_items: shape=(6,), dtype=|S14
+  block0_values: shape=(111, 6), dtype=float64
+img_175008/
+  axis0: shape=(6,), dtype=|S14
+  axis1: shape=(31,), dtype=int64
+  block0_items: shape=(6,), dtype=|S14
+  block0_values: shape=(31, 6), dtype=float64
+img_175009/
+  axis0: shape=(6,), dtype=|S14
+  axis1: shape=(9,), dtype=int64
+  block0_items: shape=(6,), dtype=|S14
+  block0_values: shape=(9, 6), dtype=float64
